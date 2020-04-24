53b4ca3385dbf0834f9bb0ef74e422e944587ae3
arjun <arjunumesh11@gmail.com>
2020-04-24 21:47:09 +0530

    grid

--- resource/gridmodel/script.js
+++ resource/gridmodel/script.js
@@ -1,6 +1,10 @@
 const grid = document.querySelector(".gridContainer");
 const resetButton = document.querySelector(".reset");
+var arr = [];
 var isdrawing = false;
+var isdest = false;
+var issource = false;
+var source, destination;
 createGrid = () => {
     for (let i = 0; i < 800; i++) {
         const div = document.createElement("div");
@@ -29,20 +33,42 @@ updateGrid = () => {
 };
 
 const square = document.querySelector("div");
-grid.addEventListener("mousedown", () => { isdrawing = true; })
+grid.addEventListener("mousedown", (e) => {
+    if (!isdest) {
+        isdest = true;
+        event.target.classList.replace("square", "destination");
+        destination = parseInt(event.target.classList[1].slice(4));
+    }
+    else if (!issource) {
+        event.target.classList.replace("square", "source");
+        source = parseInt(event.target.classList[1].slice(4));
+        issource = true;
+    }
+    if (isdest && issource)
+        isdrawing = true;
+    e.preventDefault();
+})
 grid.addEventListener("mouseup", () => { isdrawing = false; })
 square.addEventListener("mouseover", function (event) {
     if (isdrawing) {
         event.target.classList.replace("square", "color");
-        console.log(parseInt(event.target.classList[1].slice(4)))
+        arr.push(parseInt(event.target.classList[1].slice(4)))
+        console.log(arr)
+        console.log("dest " + destination);
+        console.log("source " + source);
     }
 });
 
 
 resetButton.addEventListener("click", function () {
+    issource = false;
+    isdest = false;
+    source = null;
+    destination = null;
     grid.innerHTML = "";
     grid.style.setProperty("grid-template-columns", `repeat(40, 1fr)`);
     grid.style.setProperty("grid-template-rows", `repeat(20, 1fr)`);
+    arr = [];
     createGrid();
 });
 
